b0c3cdab3ae9c814ac80c2bd48c6eb9b564d640d
JaredCallero <117484542+JaredCallero@users.noreply.github.com>
2022-11-19 16:59:35 -0800

    Better setup for SQL pull

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -13,14 +13,19 @@ if(!$conn) {
 }
 
 $sql = "select * from User"; //pull sql request from previous page here
+$result = mysqli_query($conn, $sql);
     
-if(mysqli_query($conn, $sql)){
+if(mysqli_num_rows($result) > 0){
 
     //make table here
 
+    //print column names first
+    
+    //print rest of rows.
+
 } else {
 
-    echo "Error with SQL Query";
+    echo "0 results";
 
 }
 
