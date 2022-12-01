a6aa80b82d89aa0112264a66902aa16194183c2c
Benjamin Philipose <philiposebenjamin@gmail.com>
2022-12-01 10:49:08 -0800

    Branch To setup HTML 2 PHP connections

---
+++ downloading1.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "cssql.seattleu.edu";
+$username = "ll_fpham";
+$password = "Cpsc3300Fpham!";
+$dbname = "ll_fpham";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully.<br>";
+
+$sql = 'SELECT * FROM Downloading';
+$result = mysqli_query($conn, $sql);
+
+echo"<table border = '1'>";
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+	echo "<tr>
+	<td>Media_ID</td>
+	<td>User_ID</td>
+	<tr>";
+  while($row = mysqli_fetch_assoc($result)){
+    echo 
+		"<tr>
+		<td>{$row['Media_ID']}</td>
+		<td>{$row['User_ID']}</td>
+		<tr>";
+  }
+} else {
+  echo "0 results";
+}
+echo"</table>";
+mysqli_close($conn);
+?>
